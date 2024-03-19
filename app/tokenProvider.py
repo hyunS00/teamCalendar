7a1dc930451ed1a5bb0d17fd64350b7cb98a4507
kssumin <201566@jnu.ac.kr>
2024-03-19 20:31:46 +0900

    feat : username claim에 추가

--- app/tokenProvider.py
+++ app/tokenProvider.py
@@ -4,11 +4,10 @@ from flask_jwt_extended import create_access_token
 def provide(userId, userName):
     expiredTime = datetime.today() + timedelta(minutes=60 * 60 * 24)
     payload = {
-        'id': userId,
         'username' : userName,
         'exp': expiredTime
     }
         
     # 토큰을 발급한다.
-    token = create_access_token(identity=userId)
+    token = create_access_token(identity=userId, additional_claims=payload)
     return token, expiredTime
