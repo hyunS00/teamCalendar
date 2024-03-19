040cface81b5450b4230516f005660a1c2a2e8d9
kssumin <201566@jnu.ac.kr>
2024-03-19 22:01:57 +0900

    fix : 필요없는 파일 제거

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from app.signup import signup_routes
 from app.group import group_routes
 from app.lobby import lobby_routes
 from app.login import login_routes
-from app.schedule import scedul_routes
+from app.schedule import schedule_routes
 
 from flask_jwt_extended import JWTManager
-from app.config import app
 from app.authConstant import SECRET_KEY
 
 app = Flask(__name__)
@@ -24,8 +23,8 @@ app.add_url_rule('/login', 'login_post', login_routes['login_post'], methods=['P
 app.add_url_rule('/group','group',group_routes['group'],methods=['GET'])
 app.add_url_rule('/create_group','create_group',group_routes['create_group'],methods=['POST'])
 app.add_url_rule('/lobby','lobby',lobby_routes['lobby'],methods=['GET'])
-app.add_url_rule('/schedule','schedul',scedul_routes['schedul'],methods=["GET"])
-
+app.add_url_rule('/schedule','schedule',schedule_routes['schedule'],methods=["GET"])
+app.add_url_rule('/schedule','schedule_post',schedule_routes['schedule_post'],methods=["POST"])
 
 if __name__ == '__main__':
     app.run('0.0.0.0', debug=True, port=4000)
--- app/config.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
-from app.authConstant import SECRET_KEY
-
-app = Flask(__name__)
-jwt= JWTManager(app)
-app.config['JWT_SECRET_KEY'] = SECRET_KEY
