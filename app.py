83120e9ce2ca11d70fd63f0b52fe491d3ae886eb
kssumin <201566@jnu.ac.kr>
2024-03-19 17:30:36 +0900

    feat : 로그인 기능 구현

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 # app.py
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+from flask import Flask
 from app.signup import signup_routes
 from app.group import group_routes
+from app.lobby import lobby_routes
+from app.login import login_routes
 
 app = Flask(__name__)
 
 # 라우트 등록
 app.add_url_rule('/signup', 'signup', signup_routes['signup'] , methods=['GET'])
 app.add_url_rule('/signup', 'signup_post', signup_routes['signup_post'], methods=['POST'])
+
+app.add_url_rule('/login', 'login', login_routes['login'] , methods=['GET'])
+app.add_url_rule('/login', 'login_post', login_routes['login_post'], methods=['POST'])
+
 app.add_url_rule('/group','group',group_routes['group'],methods=['GET'])
 app.add_url_rule('/create_group','create_group',group_routes['create_group'],methods=['POST'])
-
+app.add_url_rule('/lobby','lobby',lobby_routes['lobby'],methods=['GET'])
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
