73039a7bde8961a3ee1ffa7b9747ebfa80e3849a
kssumin <201566@jnu.ac.kr>
2024-03-20 13:56:20 +0900

    refactor : 애러 함수 사용하도록 변경

--- app/group.py
+++ app/group.py
@@ -1,8 +1,12 @@
-from flask import render_template, request, redirect, url_for,jsonify
+from flask import render_template, request, redirect, url_for,jsonify, make_response
 from app.db import db
 from passlib.hash import pbkdf2_sha256
 import uuid
 from app.checkLogin import is_member
+from app.errorResponse import not_found_error
+from typing import Final
+
+NOT_FOUND_GROUP: Final = "해당하는 방이 없습니다."
 
 @is_member
 def group(username, userUUID):
@@ -12,22 +16,26 @@ def group(username, userUUID):
 def create_group(username, userUUID):
     title_receive = request.form['title']
     start_date_receive = request.form['date']
-    group_uuid = str(uuid.uuid4()).split('-')[0] 
+    group_code = str(uuid.uuid4()).split('-')[0] 
     group_data = {
-        'title' : title_receive, 'date' : start_date_receive, 'userUUID' : userUUID, 'group_uuid': group_uuid
+        'title' : title_receive, 'date' : start_date_receive, 'userUUID' : userUUID, 'group_uuid': group_code
     }
 
     db.groups.insert_one(group_data)
-    return redirect(url_for("schedule", group_code=group_uuid))
+    return redirect(url_for("schedule", group_code=group_code))
 
 @is_member
 def find_group(username, userUUID):   
-    group_code = request.args.get("group_code")
-    group = db.groups.find_one({"group_code" : group_code})
-    if group is None:
-        redirect(url_for("find_group"))
+    data = request.json
+    group_code = data.get("group_code")
+
+    group = db.groups.find_one({"group_uuid" : group_code})
     
-    return redirect(url_for("schedule", group_code=group_code))
+    if group is None:
+        not_found_error(NOT_FOUND_GROUP)
+    
+    else:
+        return redirect(url_for("schedule", group_code=group_code))
 
 group_routes = {
     'group': group,
