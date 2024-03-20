712f481d3c2849f08e8de7f6639ff0a549fe0f85
kssumin <201566@jnu.ac.kr>
2024-03-20 17:15:31 +0900

    에러 분기 처리

--- app/errorResponse.py
+++ app/errorResponse.py
@@ -3,5 +3,6 @@ from app import errorConstant
 
 def not_found_error(msg):
     response = make_response(jsonify({"msg": msg, "status" : errorConstant.NOT_FOUND}))
+    response.status_code=errorConstant.NOT_FOUND
     response.headers['Content-Type'] = 'application/json'
     return response
--- app/successReponse.py
+++ app/successReponse.py
@@ -1,10 +1,14 @@
 from flask import (make_response, jsonify)
 from app import errorConstant
 from typing import Final
+from http import HTTPStatus
 
 SUCCESS_STATUS_CODE: Final = 200
 
 def success():
-    response = make_response(SUCCESS_STATUS_CODE)
+    response = make_response()
+    response.status_code=HTTPStatus.OK
     response.headers['Content-Type'] = 'application/json'
+    
     return response
+
