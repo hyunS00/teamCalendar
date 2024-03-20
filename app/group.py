6876c11cfa46485ab3d7e73f3c8e9c5fbd661f51
kssumin <201566@jnu.ac.kr>
2024-03-20 10:16:44 +0900

    fix : group_code에 따라 방 반환하도록 변경

--- app/group.py
+++ app/group.py
@@ -5,7 +5,7 @@ import uuid
 from app.checkLogin import is_member
 
 @is_member
-def group(username, userId):
+def group(username, userUUID):
     return render_template('group.html', username=username)
 
 @is_member
@@ -17,16 +17,15 @@ def create_group(username, userUUID):
         'title' : title_receive, 'date' : start_date_receive, 'userUUID' : userUUID, 'group_uuid': group_uuid
     }
 
-    result = db.groups.insert_one(group_data)
-    group_code = str(result.inserted_id)
-    return redirect(url_for("schedul", title=title_receive, groupId=group_code))
+    db.groups.insert_one(group_data)
+    return redirect(url_for("schedul", title=title_receive, group_code=group_uuid))
 
 @is_member
 def find_group(username, userUUID):   
     # 방 찾은 이후에 해당 방 스케줄로 들어가기
     group_code = request.form['group_code']
-    group = db.groups.find_one({"_id" : group_code})
-    return render_template("schedul.html", groupId= group_code)
+    group = db.groups.find_one({"groupd_code" : group_code})
+    return render_template("schedul.html", group_code= group_code)
 
 group_routes = {
     'group': group,
